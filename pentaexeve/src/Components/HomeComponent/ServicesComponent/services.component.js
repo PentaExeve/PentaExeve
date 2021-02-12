d5650ba7546e93e8582d9972f9d77b2f8ec2bb77
SenuraJayadeva <senurajayadeva@gmail.com>
2021-02-12 19:39:15 +0530

    changes

--- pentaexeve/src/Components/HomeComponent/ServicesComponent/services.component.js
+++ pentaexeve/src/Components/HomeComponent/ServicesComponent/services.component.js
@@ -16,13 +16,13 @@ export default function ServicesComponent() {
         <div class="container">
           <div class="row">
             <div class="col-md-6 col-lg-3 text-center">
-              <div class="mx-auto service-box mt-5">
-                <i
-                  class="fa fa-diamond fa-4x text-primary mb-3 sr-icons"
-                  data-aos="zoom-in"
-                  data-aos-duration="200"
-                  data-aos-once="true"
-                ></i>
+              <div
+                class="mx-auto service-box mt-5"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="600"
+              >
+                <i class="fa fa-diamond fa-4x text-primary mb-3 sr-icons"></i>
                 <h3 class="mb-3">Sturdy Templates</h3>
                 <p class="text-muted mb-0">
                   Our templates are updated regularly so they don't break.
@@ -30,14 +30,13 @@ export default function ServicesComponent() {
               </div>
             </div>
             <div class="col-md-6 col-lg-3 text-center">
-              <div class="mx-auto service-box mt-5">
-                <i
-                  class="fa fa-paper-plane fa-4x text-primary mb-3 sr-icons"
-                  data-aos="zoom-in"
-                  data-aos-duration="200"
-                  data-aos-delay="200"
-                  data-aos-once="true"
-                ></i>
+              <div
+                class="mx-auto service-box mt-5"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="600"
+              >
+                <i class="fa fa-paper-plane fa-4x text-primary mb-3 sr-icons"></i>
                 <h3 class="mb-3">Ready to Ship</h3>
                 <p class="text-muted mb-0">
                   You can use this theme as is, or you can make changes!
@@ -45,14 +44,13 @@ export default function ServicesComponent() {
               </div>
             </div>
             <div class="col-md-6 col-lg-3 text-center">
-              <div class="mx-auto service-box mt-5">
-                <i
-                  class="fa fa-newspaper-o fa-4x text-primary mb-3 sr-icons"
-                  data-aos="zoom-in"
-                  data-aos-duration="200"
-                  data-aos-delay="400"
-                  data-aos-once="true"
-                ></i>
+              <div
+                class="mx-auto service-box mt-5"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="600"
+              >
+                <i class="fa fa-newspaper-o fa-4x text-primary mb-3 sr-icons"></i>
                 <h3 class="mb-3">Up to Date</h3>
                 <p class="text-muted mb-0">
                   We update dependencies to keep things fresh.
@@ -60,14 +58,13 @@ export default function ServicesComponent() {
               </div>
             </div>
             <div class="col-md-6 col-lg-3 text-center">
-              <div class="mx-auto service-box mt-5">
-                <i
-                  class="fa fa-heart fa-4x text-primary mb-3 sr-icons"
-                  data-aos="fade"
-                  data-aos-duration="200"
-                  data-aos-delay="600"
-                  data-aos-once="true"
-                ></i>
+              <div
+                class="mx-auto service-box mt-5"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="600"
+              >
+                <i class="fa fa-heart fa-4x text-primary mb-3 sr-icons"></i>
                 <h3 class="mb-3">Made with Love</h3>
                 <p class="text-muted mb-0">
                   You have to make your websites with love these days!
